2a8ff85c18fc2ccd90cddec2e4b0037975d214c9
M1AN1M3 <jack.cedeno@gmail.com>
2018-04-07 23:29:42 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,17 +1,23 @@
 const roles = require(`../Moderation/roles.json`) 
 const Discord = require(`discord.js`);
 
-module.exports.run = async (bot, message, args) => {
-    console.log(message.mentions.members.first());
+module.exports.run = async (bot, message, args) => { 
+    let toMute = message.mentions.members.first();
+
+    if(!message.member.hasPermission(`MANAGE_MESSAGES`)){
+        message.channel.send(`Improper Clearance`);
+        return;
+    };
+
+    if(!toMute) return message.channel.send(`Undefined mute value.`)
+
+    if(toMute === message.member) return message.channel.send(`Cannot mute self.`)
+
+    toMute.addRole(roles.Muted)
+
+    message.author.send(`muted ${toMute}.`)
 }
 
 module.exports.help = {
     name: `mute`,
-    name: `Mute`  
 };          
-// if(!message.member.hasPermission(`MUTE_MEMBERS`)){
-//    message.channel.send(`You do not have proper permissions to execute command "Mute".`);
-//    return; 
-//} else {
-//    message.mentions.members.addRole(roles.Muted)
-//}
